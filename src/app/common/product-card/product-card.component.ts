246abc74ee736b7089ba604cab23df3dacb7e0d3
urbancsokgy <urbancsokgy@gmail.com>
2021-01-30 15:56:50 +0100

    Egyszerűsítve

--- src/app/common/product-card/product-card.component.ts
+++ src/app/common/product-card/product-card.component.ts
@@ -11,26 +11,19 @@ export class ProductCardComponent implements OnInit {
 // pld: `<app-product-card [product]="product"></app-product-card>`
 // - Statikus komponens, azaz nem kell bele output tulajdonság (egyenlőre :)))) -->
   @Input() product: Product=new Product();
-  itemImageUrl: string;
-  itemName: string ="Flector";
-  itemDescription: string;
-  itemStock: number;
-  itemPrice:number;
-constructor(product: Product){
-  this.itemImageUrl=product.image|| "https://pirulapatika.hu/nagykep/210043735.jpg";
-  this.itemName=product.name || 'Flector'
-  this.itemDescription= product.description || 'Ez egy gyógyszer';
-  this.itemStock= product.stock || 3;
-  this.itemPrice=product.price || 4500;
+  // itemImageUrl: string;
+  // itemName: string ="Flector";
+  // itemDescription: string;
+  // itemStock: number;
+  // itemPrice:number;
+ constructor(){
+  // this.itemImageUrl=product.image|| "https://pirulapatika.hu/nagykep/210043735.jpg";
+  // this.itemName=product.name || 'Flector'
+  // this.itemDescription= product.description || 'Ez egy gyógyszer';
+  // this.itemStock= product.stock || 3;
+  // this.itemPrice=product.price || 4500;
 }
-  // constructor(itemImageUrl: string, itemName: string, itemDescription: string, itemStock: number, itemPrice:number) { 
-  // this.itemImageUrl=itemImageUrl || "https://pirulapatika.hu/nagykep/210043735.jpg";
-  // this.itemName=itemName || 'Flector';
-  // this.itemDescription=itemDescription || 'Lorem ipsum dolore';
-  // this.itemStock=itemStock || 2;
-  // this.itemPrice=itemPrice || 15000 ;
-  // }
-  // oneCard : ProductCardComponent=new ProductCardComponent();
+ 
 
   ngOnInit(): void {
   }
